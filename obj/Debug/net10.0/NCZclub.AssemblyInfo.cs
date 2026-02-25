ceca6f532d894baa5a657d7adcdba2d1479180a7
WCQZ2025C3fcc <18889651509@163.com>
2026-02-25 13:17:08 +0800

    add some new things

--- obj/Debug/net10.0/NCZclub.AssemblyInfo.cs
+++ obj/Debug/net10.0/NCZclub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NCZclub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ffd8090958b500919ac8ec9eb5ccbfe756916a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49849ae885785a31dcd200f3768da29a10debb57")]
 [assembly: System.Reflection.AssemblyProductAttribute("NCZclub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NCZclub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
